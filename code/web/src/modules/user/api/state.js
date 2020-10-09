9869764f6d29ebd9e27064c8fb2b76f1b9770197
Erin Untermeyer <aerinuntermeyer@gmail.com>
2020-10-09 08:51:54 -0600

    Add notes for user reducer

--- code/web/src/modules/user/api/state.js
+++ code/web/src/modules/user/api/state.js
@@ -10,6 +10,14 @@ export const userInitialState = {
   details: null
 }
 
+/*
+this is the reducer for user
+takes in default state and an action
+the action comes from the action creator (in actions.js)
+the action === type === case
+this returns that reducers part of the store
+*/
+
 // State
 export default (state = userInitialState, action) => {
   switch (action.type) {
@@ -47,3 +55,14 @@ export default (state = userInitialState, action) => {
       return state
   }
 }
+
+/*
+Will need to add cases for:
+ADD_PROFILE_PIC
+UPDATE_PROFILE_PIC
+ADD_PERS_DESC
+UPDATE_PERS_DESC
+UPDATE_EMAIL
+ADD_SHIPPING_ADDRESS
+UPDATE_SHIPPING_ADDRESS
+*/
