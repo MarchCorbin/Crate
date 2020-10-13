474f43c3d911b4067738646d1124a8b148539ab3
Gaby Mendez <gmendez90@gmail.com>
2020-10-13 13:06:02 -0400

    Add test to database.json, fill out query.test

--- code/api/src/modules/user/query.test.js
+++ code/api/src/modules/user/query.test.js
@@ -1,5 +1,35 @@
+// require testing DSL for HTTP requests
+import request from 'supertest';
+// get your server
+import express from 'express';
+// set up your graphql middleware
+import graphqlHTTP from 'express-graphql';
+import schema from '../../setup/schema';
+
 describe("user queries", () => {
-  it("is true", () => {
-    expect(true).toBe(true)
+  let server;
+  // test setup
+  beforeAll(() => {
+    // start your server
+    server = express();
+
+    server.use(
+      '/',
+      graphqlHTTP({
+        schema: schema,
+        graphiql: false,
+      })
+    );
   })
-})
+
+  it("users - returns all users", async () => {
+    const response = await request(server)
+      .post('/')
+      .send({ query: '{ users { name email } }' })
+      .expect(200)
+
+    expect(response.body.data.users.length).toEqual(2)
+    expect(response.body.data.users[0].id).toEqual(1)
+  })
+
+});
