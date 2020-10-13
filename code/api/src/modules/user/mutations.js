3d8989228741ef50e2b36fff6d5dc73ae7c256ab
Kwibe Merci <jeremiahkwibe@hotmail.com>
2020-10-12 22:17:07 -0600

    Add an updateUser mutation

--- code/api/src/modules/user/mutations.js
+++ code/api/src/modules/user/mutations.js
@@ -3,7 +3,7 @@ import { GraphQLString, GraphQLInt } from 'graphql'
 
 // App Imports
 import { UserType } from './types'
-import { create, remove } from './resolvers'
+import { create, remove, update } from './resolvers'
 
 // Create
 export const userSignup = {
@@ -37,4 +37,37 @@ export const userRemove = {
     }
   },
   resolve: remove
+}
+
+// update
+
+export  const updateUser = {
+  type: UserType,
+  args: {
+    name: {
+      name: 'name',
+      type: GraphQLString
+    },
+
+    email: {
+      name: 'email',
+      type: GraphQLString
+    },
+
+    description: {
+      name: 'description',
+      type: GraphQLString
+    },
+
+    address: {
+      name: 'address',
+      type: GraphQLString
+    },
+
+    image: {
+      name: 'image',
+      type: GraphQLString
+    }
+  },
+  resolve: update
 }
