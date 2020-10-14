d2c88a69b2060639895aace628888b26d9f5a471
Corbin March <marchcorbin@gmail.com>
2020-10-14 16:26:02 -0600

    Added a reducer to query updated user info to display #44

--- code/web/src/modules/user/api/state.js
+++ code/web/src/modules/user/api/state.js
@@ -1,6 +1,6 @@
 // App Imports
 import { isEmpty } from '../../../setup/helpers'
-import { SET_USER, LOGIN_REQUEST, LOGIN_RESPONSE, LOGOUT } from './actions'
+import { SET_USER, LOGIN_REQUEST, LOGIN_RESPONSE, LOGOUT, GET_USER_DETAILS } from './actions'
 
 // Initial State
 export const userInitialState = {
@@ -41,6 +41,12 @@ export default (state = userInitialState, action) => {
         isLoading: false,
         isAuthenticated: false,
         details: null
+      }
+
+    case GET_USER_DETAILS:
+      return {
+        ...state,
+        details: action.user
       }
 
     default:
