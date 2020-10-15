8bdaa5e45f7e43bb237a13841ec0b97a9855579f
Kwibe Merci <jeremiahkwibe@hotmail.com>
2020-10-14 21:14:39 -0600

    Add Order model with associations.

---
+++ code/api/src/modules/orders/model.js
@@ -0,0 +1,23 @@
+'use strict'
+
+// User
+module.exports = function(sequelize, DataTypes) {
+    let Order = sequelize.define('orders', {
+        userId: {
+            type: DataTypes.INTEGER
+        },
+        shippingDate: {
+            type: DataTypes.Date
+        },
+        crateId: {
+            type: DataTypes.INTEGER
+        }
+    })
+
+    Order.associate = function(models) {
+        Order.belongsTo(models.User)
+        Order.belongsTo(models.Crate)
+    }
+
+    return Order
+}
