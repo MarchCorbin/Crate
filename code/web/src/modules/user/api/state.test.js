efa14eaafde6188cacc9ff57e8332afe05aecba2
Nicole Latifi <Nicole.Latifi@gmail.com>
2020-10-15 15:26:36 -0600

    Test for updating state #48

--- code/web/src/modules/user/api/state.test.js
+++ code/web/src/modules/user/api/state.test.js
@@ -19,15 +19,29 @@ describe('userReducer', () => {
 
   it('should return the updated state', () => {
     // Setup
-    const expected = {
+    const initialState = {
       "details": null,
       "error": null,
       "isAuthenticated": false,
       "isLoading": false,
     }
 
+    const user = { name: "Bob" }
+
+    const action = {
+      type: "AUTH/GET_USER_DETAILS", 
+      user
+     }
+
+    const expected = {
+      "details": user,
+      "error": null,
+      "isAuthenticated": false,
+      "isLoading": false,
+    }
+
     // Execution
-    const result = userReducer(undefined, {});
+    const result = userReducer(initialState, action);
 
     // Expectation
     expect(result).toEqual(expected);
