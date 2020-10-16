2eb29a5163804a1c4997575f764de16456a84f2d
Gaby Mendez <gmendez90@gmail.com>
2020-10-15 20:50:45 -0400

    Include model Product in order/resolvers #getByUser

--- code/api/src/modules/order/resolvers.js
+++ code/api/src/modules/order/resolvers.js
@@ -22,6 +22,7 @@ export async function getByUser(parentValue, {}, { auth }) {
       include: [
         {model: models.User, as: 'user'},
         {model: models.Crate, as: 'crate'},
+        {model: models.Product, as: 'product'}
       ]
     })
   } else {
