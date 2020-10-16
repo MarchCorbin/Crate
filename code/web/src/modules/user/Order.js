6cd7e8ecf631b4700c8a6313a0d0b6a075af30d0
Erin Untermeyer <aerinuntermeyer@gmail.com>
2020-10-15 19:58:39 -0600

    Build initial shell for Order component #64

---
+++ code/web/src/modules/user/Order.js
@@ -0,0 +1,17 @@
+// Imports
+import React from 'react'
+import PropTypes from 'prop-types'
+
+// Component
+const Order = (props) => (
+	<section>
+		Hello World
+	</section>
+)
+
+// Component Properties
+Order.propTypes = {
+	// user: PropTypes.object.isRequired,
+}
+
+export default Order
