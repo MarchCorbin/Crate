dca1342455416410c49ec88a5b4bcc34d0c9e5fe
Kwibe Merci <jeremiahkwibe@hotmail.com>
2020-10-15 18:14:59 -0600

    Add the context value to the graphQLHTTP

--- code/api/src/modules/user/tests/mutations.test.js
+++ code/api/src/modules/user/tests/mutations.test.js
@@ -4,35 +4,65 @@
 import request from 'supertest';
 // get your server
 import express from 'express';
-// set up your graphql middleware
+import bcrypt from "bcrypt";
 import graphqlHTTP from 'express-graphql';
+
+// set up your graphql middleware
 import schema from '../../../setup/schema';
 import models from '../../../setup/models';
+import serverConfig from "../../../config/server.json";
+import authentication from "../../../setup/authentication";
+
 
 describe("user mutations", () => {
 
-  let server;
 
-  beforeAll(() => {
+  let server;
+  let token;
+
+  beforeAll(async () => {
+      const passwordHashed = await bcrypt.hash("password", serverConfig.saltRounds)
+
+      await models.User.create({
+          name: 'Gaby Mendez',
+          email: 'gaby@crate.com',
+          password: passwordHashed,
+          role: "USER"
+      });
+
+      let userData = await models.User.findOne({where: {email: 'gaby@crate.com'}})
+      let user = userData.get();
 
     server = express();
+    server.use(authentication);
     server.use(
       '/',
       graphqlHTTP({
-        schema: schema,
+        schema,
         graphiql: false,
+          context: {
+              auth: {
+                  user: user,
+                  isAuthenticated: user && user.id > 0
+              }
+          }
       })
     );
 
-    const gaby = models.User.create({
-        name: 'Gaby Mendez',
-        email: 'gaby@crate.com',
-        password: 'password',
-        role: "USER",
-        createdAt: new Date(),
-        updatedAt: new Date()
-      });
+
+
+      // Logging in the user and setting up the authentication
+
+      const response = await request(server)
+          .post('/')
+          .send({ query: '{ userLogin(email: "gaby@crate.com", password: "password") { token }}'})
+          .expect(200)
+
+      token = response.body.data.userLogin.token
   })
+    afterAll(async ()=>{
+        // models.User.drop()
+    })
 
   // using async and await
   it("user - returns user with specific id", async() => {
@@ -41,7 +71,7 @@ describe("user mutations", () => {
       .send({ query: '{ user(id: 3) { name email }}'})
       .expect(200)
 
-    console.log(response.body)
+    // console.log(response.body)
     expect(response.body.data.user.name).toEqual('Gaby Mendez')
   })
 
@@ -54,16 +84,16 @@ describe("user mutations", () => {
   //     console.log(response.body)
   // };
 
-  xit("updateUser - updates user profile info", async() => {
+  it("updateUser - updates user profile info", async(done) => {
     const response = await request(server)
       .post('/')
-      .headers({
-        'Authorization': 'Bearer'
-      })
-      .send({ mutation: '{ userUpdate(description: "description", email: "gaby@hotmail.com") {name email description address }}'})
+      .send({ query: 'mutation { userUpdate(description: "description", email: "gaby@hotmail.com") {name email description address }}'})
+        .set('Authorization', `Bearer ${token}`)
       .expect(200)
 
-    expect(response.body.data.user.name).toEqual('')
+      expect(response.body.data.userUpdate.description).toEqual("description")
+      expect(response.body.data.userUpdate.email).toEqual("gaby@hotmail.com")
+      done();
   })
 
   // TEST TEARDOWN
