471640e9b40f8483dc34b4225db89f9fdd670385
Gaby Mendez <gmendez90@gmail.com>
2020-10-15 20:11:57 -0400

    Import getAll resolver to order/query.js

--- code/api/src/modules/order/query.js
+++ code/api/src/modules/order/query.js
@@ -3,7 +3,7 @@ import { GraphQLInt, GraphQLList } from 'graphql'
 
 // App Imports
 import OrderType from './types'
-import { getByUser, get } from './resolvers'
+import { getByUser, get, getAll } from './resolvers'
 
 // Orders by user
 export const ordersByUser = {
