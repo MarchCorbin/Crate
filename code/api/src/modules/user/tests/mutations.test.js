a23d3ea1ff095e97e8d63799fdd5d4cdec597fc2
Kwibe Merci <jeremiahkwibe@hotmail.com>
2020-10-15 21:57:17 -0600

    Add afterall to delete data in the user model after testing.

--- code/api/src/modules/user/tests/mutations.test.js
+++ code/api/src/modules/user/tests/mutations.test.js
@@ -17,85 +17,85 @@ import authentication from "../../../setup/authentication";
 describe("user mutations", () => {
 
 
-  let server;
-  let token;
+    let server;
+    let token;
 
-  beforeAll(async () => {
-      const passwordHashed = await bcrypt.hash("password", serverConfig.saltRounds)
+    let USER;
 
-      await models.User.create({
-          name: 'Gaby Mendez',
-          email: 'gaby@crate.com',
-          password: passwordHashed,
-          role: "USER"
-      });
+    beforeAll(async () => {
+        const passwordHashed = await bcrypt.hash("password", serverConfig.saltRounds)
 
-      let userData = await models.User.findOne({where: {email: 'gaby@crate.com'}})
-      let user = userData.get();
+        await models.User.create({
+            name: 'Gaby Mendez',
+            email: 'gaby@crate.com',
+            password: passwordHashed,
+            role: "USER"
+        });
 
-    server = express();
-    server.use(authentication);
-    server.use(
-      '/',
-      graphqlHTTP({
-        schema,
-        graphiql: false,
-          context: {
-              auth: {
-                  user: user,
-                  isAuthenticated: user && user.id > 0
-              }
-          }
-      })
-    );
+        let userData = await models.User.findOne({where: {email: 'gaby@crate.com'}})
+        USER = userData.get();
+
+        server = express();
+        server.use(authentication);
+        server.use(
+            '/',
+            graphqlHTTP({
+                schema,
+                graphiql: false,
+                context: {
+                    auth: {
+                        user: USER,
+                        isAuthenticated: USER && USER.id > 0
+                    }
+                }
+            })
+        );
 
 
 
-      // Logging in the user and setting up the authentication
+        // Logging in the user and setting up the authentication
 
-      const response = await request(server)
-          .post('/')
-          .send({ query: '{ userLogin(email: "gaby@crate.com", password: "password") { token }}'})
-          .expect(200)
+        const response = await request(server)
+            .post('/')
+            .send({ query: '{ userLogin(email: "gaby@crate.com", password: "password") { token }}'})
+            .expect(200)
 
-      token = response.body.data.userLogin.token
-  })
+        token = response.body.data.userLogin.token
+    })
     afterAll(async ()=>{
-        // models.User.drop()
+        await models.User.destroy({ where: {} })
     })
 
-  // using async and await
-  it("user - returns user with specific id", async() => {
-    const response = await request(server)
-      .post('/')
-      .send({ query: '{ user(id: 3) { name email }}'})
-      .expect(200)
+    // using async and await
+    it("user - returns user with specific id", async() => {
+        const response = await request(server)
+            .post('/')
+            .send({ query: `{ user(id: ${USER.id}) { name email }}`})
+            .expect(200)
 
-    // console.log(response.body)
-    expect(response.body.data.user.name).toEqual('Gaby Mendez')
-  })
+        expect(response.body.data.user.name).toEqual(USER.name)
+    })
 
-  // const token = async() => {
-  //   const response = await request(server)
-  //     .post('/')
-  //     .send({ query: '{ userLogin(email: "gaby@crate.com", password: "password", role: "USER") { token }}'})
-  //     .expect(200)
-  //
-  //     console.log(response.body)
-  // };
+    // const token = async() => {
+    //   const response = await request(server)
+    //     .post('/')
+    //     .send({ query: '{ userLogin(email: "gaby@crate.com", password: "password", role: "USER") { token }}'})
+    //     .expect(200)
+    //
+    //     console.log(response.body)
+    // };
 
-  it("updateUser - updates user profile info", async(done) => {
-    const response = await request(server)
-      .post('/')
-      .send({ query: 'mutation { userUpdate(description: "description", email: "gaby@hotmail.com") {name email description address }}'})
-        .set('Authorization', `Bearer ${token}`)
-      .expect(200)
+    it("updateUser - updates user profile info", async(done) => {
+        let email = 'gaby@hotmail.com'
+        let description = 'description'
+        const response = await request(server)
+            .post('/')
+            .send({ query: `mutation { userUpdate(description: "${description}", email: "${email}") {name email description address }}`})
+            .set('Authorization', `Bearer ${token}`)
+            .expect(200)
 
-      expect(response.body.data.userUpdate.description).toEqual("description")
-      expect(response.body.data.userUpdate.email).toEqual("gaby@hotmail.com")
-      done();
-  })
-
-  // TEST TEARDOWN
-
+        expect(response.body.data.userUpdate.description).toEqual("description")
+        expect(response.body.data.userUpdate.email).toEqual("gaby@hotmail.com")
+        done();
+    })
 })
