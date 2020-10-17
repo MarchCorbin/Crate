88c22ef90cac44f80a331f7a31cda9c26047bdaa
Kwibe Merci <jeremiahkwibe@hotmail.com>
2020-10-17 11:19:32 -0600

    Add query test for the user.

--- code/api/src/modules/user/tests/query.test.js
+++ code/api/src/modules/user/tests/query.test.js
@@ -7,13 +7,32 @@ import express from 'express';
 // set up your graphql middleware
 import graphqlHTTP from 'express-graphql';
 import schema from '../../../setup/schema';
+import bcrypt from "bcrypt";
+import serverConfig from "../../../config/server.json";
+import models from "../../../setup/models";
 
 describe("user queries", () => {
   // this variable needs to be outside of the beforeAll block
   // in javascript. In Ruby it wouldn't matter.
   let server;
+  let USER;
   // test setup
-  beforeAll(() => {
+  beforeAll(async () => {
+    const passwordHashed = await bcrypt.hash("password", serverConfig.saltRounds)
+
+    USER = await models.User.create({
+      name: 'Gaby Mendez',
+      email: 'gaby@crate.com',
+      password: passwordHashed,
+      role: "USER"
+    });
+
+    await models.User.create({
+      name: 'Kwibe Merci',
+      email: 'kwibe@crate.com',
+      password: passwordHashed,
+      role: "ADMIN"
+    });
     // start your server
     server = express();
 
@@ -26,24 +45,39 @@ describe("user queries", () => {
     );
   })
 
-  xit("users - returns all users", async () => {
+  afterAll(async ()=>{
+    await models.User.destroy({ where: {} })
+
+  })
+
+  it("users - returns all users", async () => {
     const response = await request(server)
       .post('/')
       .send({ query: '{ users { name email } }' })
       .expect(200)
 
-    expect(response.body.data.users.length).toEqual(2)
+    expect(response.body.data.users).toHaveLength(2)
+    expect(typeof response.body.data.users).toBe('object')
   })
 
-  xit("user - returns user with specific id", async() => {
+  it("user - returns user with specific id", async() => {
     const response = await request(server)
       .post('/')
-      .send({ query: '{ user(id: 1) { name email }}'})
+      .send({ query: `{ user(id: ${ USER.id }) { name email }}`})
       .expect(200)
 
-    console.log(response.body)
-    expect(response.body.data.user.name).toEqual('The Admin')
+    expect(response.body.data.user.name).toEqual(USER.name)
+    expect(response.body.data.user.email).toEqual(USER.email)
   })
 
-  // TEST TEARDOWN
+  it("user - an error when wrong id is passed", async() => {
+    const response = await request(server)
+      .post('/')
+      .send({ query: `{ user(id: 99999999) { name email }}`})
+      .expect(200)
+
+    expect(response.body.data.user).toBeNull()
+    expect(response.body.errors[0].message).toBe('The user does not exist')
+  })
+
 })
