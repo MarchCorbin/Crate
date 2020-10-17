d03e893b15d9947aa47e618ab5378a7e1c110eb8
Erin Untermeyer <aerinuntermeyer@gmail.com>
2020-10-16 20:48:22 -0600

    Add style and structure to Order component #64

--- code/web/src/modules/user/Order.js
+++ code/web/src/modules/user/Order.js
@@ -1,18 +1,32 @@
 // Imports
 import React from 'react'
 import PropTypes from 'prop-types'
+
+// UI Imports
 import { H5 } from '../../ui/typography'
 
+// App Imports
+import { routeImage } from '../../setup/routes'
+
 // Component
-const Order = (props) => {
-	// Get product by ID function 
+const Order = ({ order }) => {
+
+const createProductList = () => {
+	return order.products.map((product, i) => {
+	return (
+		<div key={i} style={{ display: 'flex', flexFlow: 'column', alignItems: 'center', padding: '0 1em 0 1em' }}>
+			<img src={routeImage + product.image} style={{ borderRadius: '5em', border: '1px solid black', width: '5em', height: '5em' }}/>
+			<p style={{ maxWidth: '7em', textAlign: 'center', padding: '.5em'}}>{product.name}</p>
+		</div>
+	)
+	})
+}
+
 return(	
-	<div style={{display:'flex', flexFlow:'column', border: '1px solid green'}}>
+	<div style={{ display: 'flex', flexFlow: 'column', border: '1px solid green', overflowX: 'scroll', minHeight: '19vh', width: 'fit-content' }}>
 			<H5 style={{margin:'.3em'}}>this.props.month</H5>
 			<div style={{display:'flex', margin:'.5em', justifyContent:'space-evenly'}}>
-			<section style={{borderRadius:'5em', border:'1px solid black', width:'5em', height:'5em'}}>product1</section>
-			<section style={{borderRadius:'5em', border:'1px solid black', width:'5em', height:'5em'}}>product2</section>
-			<section style={{borderRadius:'5em', border:'1px solid black', width:'5em', height:'5em'}}>product3</section>
+			{createProductList()}
 			</div>
 	</div>
 	)
@@ -20,7 +34,7 @@ return(
 
 // Component Properties
 Order.propTypes = {
-	// user: PropTypes.object.isRequired,
+	order: PropTypes.object.isRequired,
 }
 
 export default Order
